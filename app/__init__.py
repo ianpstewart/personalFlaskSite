49506eff8bab285c4ee5fda9456ed14112cbaa18
Ian Stewart <ian.stewart0406@gmail.com>
2019-07-09 21:35:47 -0400

    added database?

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@
 
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+database = SQLAlchemy(app)
 
 #this is always at the bottom to avoid circular imports
 #routes must import app variable so this avoids reciprocal imports
-from app import routes
+from app import routes, models
