ac9ce838562a2e227b6acc4ef4a1fc4c00317aea
Ian Stewart <ian.stewart0406@gmail.com>
2019-07-09 21:40:46 -0400

    changed database to db

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
-database = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 #this is always at the bottom to avoid circular imports
 #routes must import app variable so this avoids reciprocal imports
